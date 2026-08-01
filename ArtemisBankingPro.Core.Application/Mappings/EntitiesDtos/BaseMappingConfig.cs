b94fca1586c164cab5305bd90ee8a6c8eb1898da
Hanser Ventura <hanserventura123@gmail.com>
2026-08-01 18:13:06 -0400

    feat: add mapster

---
+++ ArtemisBankingPro.Core.Application/Mappings/EntitiesDtos/BaseMappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using ArtemisBankingPro.Core.Application.DTOs;
+using ArtemisBankingPro.Core.Domain.Common;
+using Mapster;
+
+namespace ArtemisBankingPro.Core.Application.Mappings.EntitiesDtos
+{
+    public class BaseMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig(typeof(BaseEntity<>), typeof(BaseDto<>));
+            config.NewConfig(typeof(BaseDto<>), typeof(BaseEntity<>));
+        }
+    }
+}
---
+++ ArtemisBankingPro.Core.Application/ServicesRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Mapster;
+using MapsterMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ArtemisBankingPro.Core.Application
+{
+    public static class ServicesRegistration
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            #region Mapster
+            var config = new TypeAdapterConfig();
+            config.Scan(Assembly.GetExecutingAssembly());
+
+            services.AddSingleton(config);
+            services.AddScoped<IMapper, ServiceMapper>();
+            #endregion
+
+            #region Services
+            #endregion
+        }
+    }
+}
