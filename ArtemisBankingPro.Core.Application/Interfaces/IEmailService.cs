528e87db96542ddb5e4d33d2863a59cf9c64e93d
Hanser Ventura <hanserventura123@gmail.com>
2026-08-01 18:21:32 -0400

    feat: add email service configuration and implementation

---
+++ ArtemisBankingPro.Core.Application/Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using ArtemisBankingPro.Core.Application.DTOs.Email;
+
+namespace ArtemisBankingPro.Core.Application.Interfaces
+{
+    public interface IEmailService
+    {
+        Task<Result> SendAsync(EmailRequestDto emailRequestDto);
+    }
+}
---
+++ ArtemisBankingPro.Infrastructure.Shared/ServicesRegistration.cs
@@ -0,0 +1,22 @@
+﻿using ArtemisBankingPro.Core.Application.Interfaces;
+using ArtemisBankingPro.Core.Domain.Settings;
+using ArtemisBankingPro.Infrastructure.Shared.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ArtemisBankingPro.Infrastructure.Shared
+{
+    public static class ServicesRegistration
+    {
+        public static void AddSharedLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Configurations
+            services.Configure<MailSettings>(configuration.GetSection("MailSettings"));
+            #endregion
+
+            #region Services
+            services.AddScoped<IEmailService, EmailService>();
+            #endregion
+        }
+    }
+}
